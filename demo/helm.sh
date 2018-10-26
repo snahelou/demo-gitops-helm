4d71bcd00ceb2708832a97158af574045f19012c
NAHELOU SÉBASTIEN <sebastien.nahelou@decathlon.com>
2018-10-26 09:56:27 +0200

    init

--- demo/helm.sh
+++ demo/helm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git push --delete origin flux-custom
+
 kubectl -n kube-system create sa tiller
 
 kubectl create clusterrolebinding tiller-cluster-rule \
@@ -8,17 +10,21 @@ kubectl create clusterrolebinding tiller-cluster-rule \
 
 helm init --skip-refresh --upgrade --service-account tiller
 
-sleep 15
+sleep 10
 
 helm repo add weaveworks https://weaveworks.github.io/flux
 
 helm install --name flux \
+--set rbac.create=true \
 --set helmOperator.create=true \
 --set git.url=ssh://git@github.com/snahelou/demo-gitops-helm \
---set helmOperator.git.chartsPath=charts \
+--set git.label=flux-custom \
+--set git.branch=master2 \
+--set registry.pollInterval="1m" \
 --namespace flux \
 weaveworks/flux
 
 sleep 15
+
 kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2
 
